b10f2557aa37d90e9a1b6a2af90209d38e02507c
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2023-07-01 12:00:53 +0200

    ci(sonar): set up sonar properties and workflow
    set up sonar properties to check source and collect lcov coverage. set up sonar workflow to run
    coverage and remove coverage run from testing workflow

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,10 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 const config = {
   verbose: true,
+  preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
   transform: {
     '^.+\\.ts?$': 'ts-jest',
   },
+  resolver: 'jest-ts-webcompat-resolver',
 };
 
 module.exports = config;
